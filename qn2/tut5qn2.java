d6735849b163dae012dc41a6db807df05f328d4a
HD Koh <hd.koh290293@gmail.com>
2016-03-09 14:57:36 +0800

    Update tut5qn2.java

--- qn2/tut5qn2.java
+++ qn2/tut5qn2.java
@@ -5,8 +5,12 @@ public class tut5qn2 {
     public static void main(String[] argc) {
         //classC c = (classC) new classD();
         //c.print("hello","there");       //No problen with this as classD overrides classC method of print()
+        //if class c is abstract, we are declaring abstract reference class c but instantiating object of class D
+        // compile time, look at class C method, it's okay. During run time, look at class D, print
+
         //classA a = new classC();
         //a.print(1,”there”);           //classC cannot be instantiated as it is an abstract class
+
         classA a = new classF();
         a.print("hello","there");     //Error when upcasting classA to classF. 
                                         //Overloading happens. But classA doesn't have the right parameters, causing an error to happen.
